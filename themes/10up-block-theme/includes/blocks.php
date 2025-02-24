93e5f44b48a347a539305e41e408b7d0b85004f1
Colin Swinney <colinjswinney@gmail.com>
2025-02-24 08:48:52 +0100

    Create block bindings and update template

--- themes/10up-block-theme/includes/blocks.php
+++ themes/10up-block-theme/includes/blocks.php
@@ -19,6 +19,7 @@ use function TenupBlockTheme\Utility\get_asset_info;
 function setup() {
 	add_action( 'init', 'TenupBlockTheme\Blocks\register_theme_blocks', 10, 0 );
 	add_action( 'init', 'TenupBlockTheme\Blocks\enqueue_theme_block_styles', 10, 0 );
+	add_action( 'init', 'TenupBlockTheme\Blocks\register_block_bindings', 11, 0 );
 
 	add_filter( 'render_block_core/post-featured-image', 'TenupBlockTheme\Blocks\filter_featured_image_block', 10, 3 );
 }
@@ -117,4 +118,120 @@ function filter_featured_image_block( $block_content, $block, $parsed_block ) {
 	$p->set_attribute( 'style', $style_attribute );
 
 	return $p->get_updated_html();
+}
+
+/**
+ * Register the block bindings for the theme.
+ *
+ * @return void
+ */
+function register_block_bindings() {
+
+	register_block_bindings_source(
+		'tenup/archive-link',
+		array(
+			'label'              => __( 'Archive Link', 'tenup' ),
+			'get_value_callback' => 'TenupBlockTheme\Blocks\block_binding_archive_link',
+		)
+	);
+
+	register_block_bindings_source(
+		'tenup/movie-viewer-rating-label',
+		array(
+			'label'              => __( 'Movie Viewer Rating Label', 'tenup' ),
+			'get_value_callback' => 'TenupBlockTheme\Blocks\block_binding_movie_viewer_rating_label',
+		)
+	);
+}
+
+/**
+ * Callback function for the 'tenup/archive-link' block binding.
+ * Displays the archive link for the post type.
+ *
+ * @param array $source_args The args found in the block metadata to create the binding.
+ * @return string|null The text or URL based on the key argument set in the block attributes.
+ */
+function block_binding_archive_link( $source_args ) {
+
+	if ( ! isset( $source_args['key'] ) ) {
+		return null;
+	}
+
+	$text = __( '← Back', 'tenup' );
+	$url  = get_post_type_archive_link( get_post_type() );
+
+	if ( ! $url ) {
+		return null;
+	}
+
+	switch ( $source_args['key'] ) {
+		case 'text':
+			return $text;
+		case 'url':
+			return $url;
+		default:
+			return null;
+	}
+}
+
+/**
+ * Callback function for the 'tenup/movie-viewer-rating-label' block binding.
+ * Displays the movie viewer rating label with a star rating and count. e.g. ★7.5/10 (175K)
+ *
+ * @param array $source_args The args found in the block metadata to create the binding.
+ * @return string|null The text or URL based on the key argument set in the block attributes.
+ */
+function block_binding_movie_viewer_rating_label( $source_args ) {
+
+	if ( ! isset( $source_args['key'] ) ) {
+		return null;
+	}
+
+	// Set default binding values.
+	$text = '0/10 (0)';
+	$url  = '#';
+
+	// Get post meta.
+	$rating = get_post_meta( get_the_ID(), 'tenup_movie_viewer_rating', true ) ?? false;
+	$count  = get_post_meta( get_the_ID(), 'tenup_movie_viewer_rating_count', true ) ?? false;
+
+	if ( false !== $rating && false !== $count ) {
+
+		$count_display = $count;
+
+		switch ( true ) {
+
+			// 1000 - 9999, round to the nearest hundred and format. e.g. 1156 = 1.2K
+			case ( $count >= 1000 && $count < 10000 ):
+				$count_display = number_format( round( $count, -2 ) / 1000, 1, '.', '' ) . 'K';
+				break;
+
+			// 10000+, round to the nearest thousand and format. e.g. 11560 = 12K
+			case ( $count >= 10000 ):
+				$count_display = number_format( round( $count, -3 ) / 1000, 0, '.', '' ) . 'K';
+				break;
+
+			default:
+		}
+
+		$text = $rating . '/10 (' . $count_display . ')';
+	}
+
+	$star         = '<mark style="background-color: transparent;color:#f5c518" class="has-inline-color">★</mark>';
+	$allowed_tags = [
+		'mark' => [
+			'style' => [],
+			'class' => [],
+		],
+	];
+	$text         = wp_kses( $star . $text, $allowed_tags );
+
+	switch ( $source_args['key'] ) {
+		case 'text':
+			return $text;
+		case 'url':
+			return $url;
+		default:
+			return null;
+	}
 }
